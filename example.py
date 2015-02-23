84b357ba22774edea0d8ef65fd2a32daa45c8e7e
threeears <zichennie@gmail.com>
2015-02-23 13:36:02 -0800

    Delete example.py

--- example.py
+++
@@ -1,8 +0,0 @@
-from urlparse import urlparse, parse_qsl
-
-url = 'http://localhost:8000/?userID=42839&operator=0&count=1&resourceID="323,456,3245"&key=name'
-#print parse_qsl(urlparse(url)[4])
-
-a = ['Alice', 'Ben', 'Ren']
-for item in a:
-	print item
